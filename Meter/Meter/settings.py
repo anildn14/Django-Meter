130eb04bc405c757aa3014dc9d17f7c6aaa3cc90
anildn14 <31370332+anildn14@users.noreply.github.com>
2019-07-07 23:07:50 +0530

    Update settings.py

--- Meter/Meter/settings.py
+++ Meter/Meter/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-=13+@mz2pjg6kq83n^)ig&!25wumce-+#s+m2$623yj29c@qg'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
